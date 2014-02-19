62da9f3dc79729e1bd27d4a0ca8d44f31bb5c326
Lars Holm Nielsen <lars.holm.nielsen@cern.ch>
2014-02-19 11:55:28 +0100

    github: PEP8 formatting

--- zenodo/modules/github/views/github.py
+++ zenodo/modules/github/views/github.py
@@ -1,24 +1,24 @@
 # -*- coding: utf-8 -*-
 #
-## This file is part of ZENODO.
-## Copyright (C) 2014 CERN.
-##
-## ZENODO is free software: you can redistribute it and/or modify
-## it under the terms of the GNU General Public License as published by
-## the Free Software Foundation, either version 3 of the License, or
-## (at your option) any later version.
-##
-## ZENODO is distributed in the hope that it will be useful,
-## but WITHOUT ANY WARRANTY; without even the implied warranty of
-## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-## GNU General Public License for more details.
-##
-## You should have received a copy of the GNU General Public License
-## along with ZENODO. If not, see <http://www.gnu.org/licenses/>.
-##
-## In applying this licence, CERN does not waive the privileges and immunities
-## granted to it by virtue of its status as an Intergovernmental Organization
-## or submit itself to any jurisdiction.
+# This file is part of ZENODO.
+# Copyright (C) 2014 CERN.
+#
+# ZENODO is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# ZENODO is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with ZENODO. If not, see <http://www.gnu.org/licenses/>.
+#
+# In applying this licence, CERN does not waive the privileges and immunities
+# granted to it by virtue of its status as an Intergovernmental Organization
+# or submit itself to any jurisdiction.
 
 
 """
@@ -48,7 +48,11 @@ from invenio.modules.accounts.models import User
 from invenio.modules.oauth2server.models import Token
 from invenio.modules.webhooks.models import Receiver, CeleryReceiver
 from zenodo.ext.oauth import oauth
-
+from flask.ext.login import login_required
+from invenio.ext.sslify import ssl_required
+from flask.ext.breadcrumbs import register_breadcrumb
+from flask.ext.menu import register_menu
+from invenio.base.i18n import _
 from ..models import OAuthTokens
 from ..tasks import create_deposition
 
@@ -59,7 +63,7 @@ blueprint = Blueprint(
     __name__,
     static_folder="../static",
     template_folder="../templates",
-    url_prefix="/github"
+    url_prefix="/account/settings/github",
 )
 
 
@@ -77,22 +81,28 @@ def register_webhook():
 @blueprint.app_template_filter('naturaltime')
 def naturaltime(val):
     val = parse(val)
-    now = datetime.utcnow().replace(tzinfo = pytz.utc)
+    now = datetime.utcnow().replace(tzinfo=pytz.utc)
 
     return humanize.naturaltime(now - val)
 
+
 def get_repositories(user):
     """Helper method to get a list of current user's repositories from GitHub."""
-    r = remote.get("users/%(username)s/repos?type=owner&sort=full_name" % {"username": session["github_login"]})
+    r = remote.get("users/%(username)s/repos?type=owner&sort=full_name" %
+                   {"username": session["github_login"]})
 
     repo_data = r.data
-    def get_repo_name(repo): return repo["full_name"]
-    def get_repo_data(repo): return { "hook": None, "description": repo["description"] }
+
+    def get_repo_name(repo):
+        return repo["full_name"]
+
+    def get_repo_data(repo):
+        return {"hook": None, "description": repo["description"]}
 
     repo_names = map(get_repo_name, repo_data)
     repo_data = map(get_repo_data, repo_data)
-    repos = dict( \
-        zip(repo_names, repo_data) \
+    repos = dict(
+        zip(repo_names, repo_data)
     )
 
     if user is not None:
@@ -108,28 +118,41 @@ def get_repositories(user):
         "repos": repos
     }
 
+
 @blueprint.route('/')
+@ssl_required
+@login_required
+@register_menu(
+    blueprint, 'settings.github',
+    _('<i class="fa fa-github fa-fw"></i> GitHub'),
+    order=10,
+)
+@register_breadcrumb(blueprint, '.index', _('GitHub'))
 def index():
-    context = { "connected": False }
+    context = {"connected": False}
 
     # Check if user has already authorized GitHub
     user = OAuthTokens.query \
-        .filter_by( user_id = current_user.get_id() ) \
-        .filter_by( client_id = remote.consumer_key ) \
-        .first()
+        .filter_by(
+            user_id=current_user.get_id(),
+            client_id=remote.consumer_key
+        ).first()
 
     if user is not None:
 
         # The user has previously been authenticated. Check if the token is still valid.
-        # GitHub requires the use of Basic Auth to query token validity. Valid responses return 200.
+        # GitHub requires the use of Basic Auth to query token validity. Valid
+        # responses return 200.
         r = requests.get(
-            "https://api.github.com/applications/%(client_id)s/tokens/%(access_token)s" % \
-            {"client_id": remote.consumer_key, "access_token": user.access_token},
+            "https://api.github.com/applications/%(client_id)s/tokens/%(access_token)s" %
+            {"client_id": remote.consumer_key,
+                "access_token": user.access_token},
             auth=(remote.consumer_key, remote.consumer_secret)
         )
 
         if r.status_code == 200:
-            # The user is authenticated and the token we have is still valid. Render GitHub settings page.
+            # The user is authenticated and the token we have is still valid.
+            # Render GitHub settings page.
             extra_data = user.extra_data
 
             # Add information to session
@@ -137,12 +160,12 @@ def index():
             session["github_login"] = extra_data['login']
 
             # Check the date of the last repo sync
-            last_sync = datetime.strptime( \
-                extra_data["repos_last_sync"], \
+            last_sync = datetime.strptime(
+                extra_data["repos_last_sync"],
                 "%Y-%m-%d %H:%M:%S.%f"
             )
             today = datetime.now()
-            yesterday = today - timedelta(days = 1)
+            yesterday = today - timedelta(days=1)
             if last_sync < yesterday:
                 repos = get_repositories(user)
                 user.extra_data.update(repos)
@@ -151,24 +174,30 @@ def index():
             context["connected"] = True
             context["repos"] = extra_data['repos']
             context["name"] = extra_data['login']
-            context["last_sync"] = humanize.naturaltime(datetime.now() - last_sync)
+            context["last_sync"] = humanize.naturaltime(
+                datetime.now() - last_sync)
 
     return render_template("github/index.html", **context)
 
 # Authenticated endpoint
+
+
 @blueprint.route('/connect')
 def connect():
     return remote.authorize(
-        callback = url_for('.connected', _external = True)
+        callback=url_for('.connected', _external=True)
     )
 
 # Authenticated endpoint
+
+
 @blueprint.route('/connected')
 @remote.authorized_handler
 def connected(resp):
     current_user_id = current_user.get_id()
 
-    # TODO: Better error handling. If GitHub auth fails, we'll get a Bad Request (400)
+    # TODO: Better error handling. If GitHub auth fails, we'll get a Bad
+    # Request (400)
     if resp is None:
         return 'Access denied: reason=%s error=%s' % (
             request.args['error_reason'],
@@ -181,8 +210,8 @@ def connected(resp):
 
     # Check if the user has previously created a GitHub OAuth token
     user = OAuthTokens.query \
-        .filter_by( user_id = current_user.get_id() ) \
-        .filter_by( client_id = remote.consumer_key ) \
+        .filter_by( user_id=current_user.get_id() ) \
+        .filter_by( client_id=remote.consumer_key ) \
         .first()
     if user is None:
 
@@ -203,10 +232,10 @@ def connected(resp):
 
         # Put user's GitHub info in database
         o = OAuthTokens(
-            client_id = remote.consumer_key,
-            user_id = current_user.get_id(),
-            access_token = github_token,
-            extra_data = extra_data
+            client_id=remote.consumer_key,
+            user_id=current_user.get_id(),
+            access_token=github_token,
+            extra_data=extra_data
         )
         db.session.add(o)
     else:
@@ -217,24 +246,29 @@ def connected(resp):
 
     db.session.commit()
 
-    return redirect( url_for('.index') )
+    return redirect(url_for('.index'))
 
 # TODO: Authenticated endpoint
+
+
 @blueprint.route('/remove-github-hook', methods=["POST"])
 def remove_github_hook():
     status = {"status": False}
     repo = request.json["repo"]
 
     # Get the hook id from the database
-    user = OAuthTokens.query.filter_by(user_id = current_user.get_id()).filter_by(client_id = remote.consumer_key).first()
+    user = OAuthTokens.query.filter_by(user_id=current_user.get_id()).filter_by(
+        client_id=remote.consumer_key).first()
     hook_id = user.extra_data["repos"][repo]["hook"]
 
     resp = remote.delete(
-        "repos/%(full_name)s/hooks/%(hook_id)s" % {"full_name": repo, "hook_id": hook_id},
+        "repos/%(full_name)s/hooks/%(hook_id)s" % {
+            "full_name": repo, "hook_id": hook_id},
     )
 
     if resp.status == 204:
-        # The hook has successfully been removed by GitHub, update the status and DB
+        # The hook has successfully been removed by GitHub, update the status
+        # and DB
         status["status"] = True
 
         user.extra_data["repos"][repo]["hook"] = None
@@ -244,14 +278,18 @@ def remove_github_hook():
     return json.dumps(status)
 
 # TODO: Authenticated endpoint
+
+
 @blueprint.route('/create-github-hook', methods=["POST"])
 def create_github_hook():
     status = {"status": False}
     repo = request.json["repo"]
 
-    user = OAuthTokens.query.filter_by(user_id = current_user.get_id()).filter_by(client_id = remote.consumer_key).first()
+    user = OAuthTokens.query.filter_by(user_id=current_user.get_id()).filter_by(
+        client_id=remote.consumer_key).first()
     github_login = user.extra_data["login"]
-    zenodo_token_id = Token.query.filter_by(id = user.extra_data["zenodo_token_id"]).first().access_token
+    zenodo_token_id = Token.query.filter_by(
+        id=user.extra_data["zenodo_token_id"]).first().access_token
 
     # TODO: Use Zenodo endpoint instead of Ultrahook!!!
 
@@ -270,8 +308,7 @@ def create_github_hook():
         format='json',
         data=data
     )
-    
-    
+
     if resp.status == 201:
         # Hook was created, updated the status and database
         status["status"] = True
@@ -283,11 +320,14 @@ def create_github_hook():
     return json.dumps(status)
 
 # TODO: Authenticated endpoint
+
+
 @blueprint.route('/sync', methods=["GET"])
 def sync_repositories():
 
     # Query for our current repo data
-    user = OAuthTokens.query.filter_by( user_id = current_user.get_id() ).filter_by( client_id = remote.consumer_key ).first()
+    user = OAuthTokens.query.filter_by(user_id=current_user.get_id()).filter_by(
+        client_id=remote.consumer_key).first()
     if user is None:
         return json.dumps({"state": "error syncing"})
 
@@ -295,12 +335,14 @@ def sync_repositories():
     user.extra_data.update(repos)
     db.session.commit()
 
-    return redirect( url_for('.index') )
+    return redirect(url_for('.index'))
+
 
 def create_deposition_error():
     """docstring for create_deposition_error"""
     pass
-    
+
+
 @remote.tokengetter
 def get_oauth_token():
     return session.get('github_token')
